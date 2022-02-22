e42bbda7e03e4f62719952ef435450f358055601
odc5 <mariamyoussef209@gmail.com>
2022-02-22 15:56:07 +0200

    update course model

--- Flutter/lib/modules/courses/cubit/cubit.dart
+++ Flutter/lib/modules/courses/cubit/cubit.dart
@@ -12,12 +12,13 @@ class CourseCubit extends Cubit<CourseStates> {
 
   static CourseCubit get(context) => BlocProvider.of(context);
 
-  List<CourseModel?> coursesModel =[];
+  List<CourseModel?> coursesModel = [];
+
   void getAllCoursesData() {
     emit(AllCoursesLoadingState());
     DioHelper.getData(url: courses).then((value) {
       value.data['courses'].forEach((element) {
-        coursesModel.add(CourseModel.fromJson(element)) ;
+        coursesModel.add(CourseModel.fromJson(element));
       });
       emit(AllCoursesSuccessState(coursesModel));
     }).catchError((error) {
@@ -27,14 +28,48 @@ class CourseCubit extends Cubit<CourseStates> {
   }
 
   CourseModel? courseModel;
+
   void getCourseData({required courseId}) {
     emit(CourseLoadingState());
-    DioHelper.getData(url: "$CourseModel/$courseId", token: userToken).then((value) {
+    DioHelper.getData(url: "$CourseModel/$courseId", token: userToken)
+        .then((value) {
       courseModel = CourseModel.fromJson(value.data);
       emit(CourseSuccessState(courseModel!));
     }).catchError((error) {
       emit(CourseErrorState(error.toString()));
       print(error.toString());
+    });
+  }
+
+  CourseModel? createCourseModel;
+
+  void createNewModule({
+    required String moduleName,
+    required String description,
+    required String duration,
+    // required File content,
+    required String moduleType,
+  }) {
+    emit(CreateCourseLoadingState());
+    DioHelper.postData(
+      data: {
+        'title': moduleName,
+        'description': description,
+        'requiremnets'
+            ''
+            '': duration,
+        // '':content,
+        'contentType': moduleType,
+      },
+      url: module,
+      token:
+          "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjYyMDBmZjc3MzA0OWY3ZDUyYzM3NWRhMCIsImlzQWRtaW4iOnRydWUsImlhdCI6MTY0NDY3NDEyOX0.Yf9fhh-y_HDFtmUw4EeCKhr11Xw0bGPvM2q6ehpZyQQ",
+    ).then((value) {
+      createCourseModel = CourseModel.fromJson(value.data);
+      emit(CreateCourseSuccessState(createCourseModel!));
+    }).catchError((onError) {
+      print(onError.toString());
+      emit(CreateCourseErrorState(onError.toString()));
     });
   }
 }
